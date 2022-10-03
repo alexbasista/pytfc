dad10eb979e9fe478fcb23a0780855f992a23af5
alexbasista <abasista@hashicorp.com>
2022-10-03 19:46:10 -0400

    update

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 from setuptools import find_packages
 
+VERSION = '0.0.1'
+
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name='pytfc',
-    version='0.1.0',
+    version=VERSION,
     author='Alex Basista',
     author_email='alex.basista@gmail.com',
     description='Python HTTP client library for Terraform Cloud/Enterprise API.',
@@ -15,24 +17,15 @@ setup(
     url='https://github.com/alexbasista/pytfc',
     py_modules=["pytfc"],
     packages=find_packages(),
-    python_requires='>=3.9',
     install_requires=[
         "pyhcl>=0.4.4",
         "requests>=2.26.0",
     ],
-    extras_require={
-        "dev": [
-            "pytest>=6.2.5",
-            "wheel>=0.37",
-            "setuptools>=58.2",
-            "twine>=3.4.2",
-        ],
-    },
+    keywords=['tfe', 'terraform enterprise', 'tfc', 'terraform cloud', 'terraform'],
     classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
         "Operating System :: OS Independent",
     ],
