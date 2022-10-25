0d4bb8c81aead869b99d4db01b6b632436a47fb4
alexbasista <abasista@hashicorp.com>
2022-10-25 11:29:40 -0400

    add registry modules partial

---
+++ pytfc/registry_modules.py
@@ -0,0 +1,67 @@
+"""
+Module for TFC/E Registry Modules API endpoint.
+"""
+
+
+class RegistryModules:
+    """
+    TFC/E Registry Modules methods.
+    """
+    def __init__(self, client):
+        self.client = client
+
+        self._rm_endpoint = '/'.join([self.client._base_uri_v2,
+            'organizations', self.client.org, 'registry-modules'])
+
+    def list(self, page_number=None, page_size=None, filters=None):
+        """
+        GET /organizations/:organization_name/registry-module
+        """             
+        return self.client._requestor.get(url=self._rm_endpoint,
+            page_number=page_number, page_size=page_size, filters=filters)
+
+    def publish_from_vcs(self):
+        """
+        POST /organizations/:organization_name/registry-modules/vcs
+        """
+        print('coming soon')
+
+    def create(self):
+        """
+        POST /organizations/:organization_name/registry-modules
+        """
+        print('coming soon')
+
+    def create_version(self):
+        """
+        POST /organizations/:organization_name/registry-modules/:registry_name/:namespace/:name/:provider/versions
+        """
+        print('coming soon')
+
+    def add_version(self):
+        """
+        PUT https://archivist.terraform.io/v1/object/<UNIQUE OBJECT ID>
+        """
+        print('coming soon')
+    
+    def show(self, name, namespace, provider, registry_name='private'):
+        """
+        GET /organizations/:organization_name/registry-modules/:registry_name/:namespace/:name/:provider
+        """
+        url = '/'.join([
+            self._rm_endpoint,
+            registry_name,
+            namespace,
+            name,
+            provider
+        ])
+
+        return self.client._requestor.get(url=url)
+
+    def delete(self, name, namespace, provider, registry_name='private', version=None):
+        """
+        DELETE /organizations/:organization_name/registry-modules/:registry_name/:namespace/:name
+        DELETE /organizations/:organization_name/registry-modules/:registry_name/:namespace/:name/:provider
+        DELETE /organizations/:organization_name/registry-modules/:registry_name/:namespace/:name/:provider/:version
+        """
+        print('coming soon')
