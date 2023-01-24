b700f77d3d9947f7f0c00b113ccb1d2945e82c51
alexbasista <abasista@hashicorp.com>
2023-01-24 16:50:14 -0500

    bump ver

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-VERSION = '0.0.17'
+VERSION = '0.0.18'
 
 with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
