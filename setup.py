2b31216b28e6624d8092ded2ebe40c0df4568490
alexbasista <abasista@hashicorp.com>
2023-10-09 22:20:15 -0400

    testing new test version strategy

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-VERSION = 'TAG'
+VERSION = 'TAG' # value is updated by release pipeline
 
 with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
