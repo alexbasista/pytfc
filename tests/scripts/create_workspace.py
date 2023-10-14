fdec8dd43b921dce2f90e9e0b83e5c157e59dbde
alexbasista <abasista@hashicorp.com>
2023-10-14 12:00:50 -0400

    update test script

--- tests/scripts/create_workspace.py
+++ tests/scripts/create_workspace.py
@@ -1,4 +1,5 @@
 import os
+import time
 import argparse
 import pytfc
 
@@ -6,6 +7,8 @@ TFE_HOSTNAME = os.getenv('TFE_HOSTNAME', 'app.terraform.io')
 TFE_TOKEN = os.getenv('TFE_TOKEN')
 TFE_ORG = os.getenv('TFE_ORG')
 
+def handle_list_input(arg):
+    return [item.strip() for item in arg.split(',')]
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Workspace creation arguments.')
@@ -17,8 +20,12 @@ def parse_args():
         help='Reference to VCS repository in format of :org/:repo.'),
     parser.add_argument('--oauth-token-id', dest='oauth_token_id',
         help='OAuth Token ID from VCS provider connection in TFC.'),
-    parser.add_argument('--working-directory', dest='working_directory',
-        help='Directory in repo that Workspace should be linked to.')
+    parser.add_argument('--working-directory', dest='working_directory', default=None,
+        help='Directory in repo that Workspace should be linked to.'),
+    parser.add_argument('--varset-name', dest='varset_name', default=None,
+        help='Name of Variable Set to apply to Workspace.')
+    parser.add_argument('--outputs', nargs='+', dest='outputs', default=None,
+        help='List of outputs to return after the apply.')
     args = parser.parse_args()
     return args
 
@@ -29,12 +36,70 @@ def main():
 
     project_id = client.projects.get_project_id(name=args.project_name)
 
-    new_ws = client.workspaces.create(
+    print(f"Creating workspace `{args.name}`...")
+    ws = client.workspaces.create(
         name = args.name,
         project_id = project_id,
         identifier = args.vcs_repo,
-        oauth_token_id = args.oauth_token_id
+        oauth_token_id = args.oauth_token_id,
+        working_directory = args.working_directory,
+        queue_all_runs = True
     )
+    ws_id = ws.json()['data']['id']
+
+    if args.varset_name is not None:
+        print("Fetching variable set ID...")
+        varset_id = client.variable_sets.get_varset_id(name=args.varset_name)
+        print("Applying variable set to workspace...")
+        client.variable_sets.apply_to_workspace(varset_id=varset_id, ws_id=ws_id)
+
+    while True:
+        try:
+            run_id = client.runs.get_latest_run_id(ws_id=ws_id)
+            break
+        except IndexError:
+            print("Waiting for run to start...")
+            time.sleep(2)
+    
+    while True:
+        plan_status = client.runs.show(run_id=run_id).json()['data']['attributes']['status']
+        if plan_status == 'planned':
+            break
+        print("Waiting for plan to finish...")
+        time.sleep(5)
+    
+    client.runs.apply(run_id=run_id)
+    while True:
+        apply_status = client.runs.show(run_id=run_id).json()['data']['attributes']['status']
+        if apply_status == 'applied':
+            break
+        print("Waiting for apply to finish...")
+        time.sleep(5)
+    
+    if args.outputs is not None:
+        outputs_list = [item.strip(',') for item in args.outputs]
+        while True:
+            try:
+                current_state = client.state_versions.get_current(ws_id=ws_id).json()
+                if current_state['data']['attributes']['resources-processed'] == True:
+                    break
+                print("Waiting for outputs to be processed...")
+            except Exception as e:
+                if str(e).startswith('404 Client Error: Not Found for url:'):
+                    time.sleep(2)
+                else:
+                    print(f"An unexpected exception occurred: {e}")
+                    print("Exiting script.")
+                    exit()
+
+        sv_id = current_state['data']['id']
+        sv_outputs = client.state_version_outputs.list(sv_id=sv_id).json()
+        outputs_data = {}
+        for i in sv_outputs['data']:
+            if i['attributes']['name'] in outputs_list:
+                outputs_data[i['attributes']['name']] = i['attributes']['value']
+
+        return outputs_data
 
 if __name__ == "__main__":
     main()
